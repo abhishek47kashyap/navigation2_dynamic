fe53585bea76dd89fca8858c3bddee71239afc42
Abhishek Kashyap <abhishek47kashyap@gmail.com>
2021-01-26 02:29:04 +0900

    created new pkg for 2d lidar detection

---
+++ detection_2d_lidar/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup
+
+package_name = 'detection_2d_lidar'
+
+setup(
+    name=package_name,
+    version='0.0.1',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Abhishek Kashyap, Steven Macenski',
+    maintainer_email='abhishek47kashyap@gmail.com, stevenmacenski@gmail.com',
+    description='2D LIDAR detection',
+    license='Apache-2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+        ],
+    },
+)
